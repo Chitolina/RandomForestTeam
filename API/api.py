81bc8bc1b04d172b41f5a84c94a82bd48288d0d5
Lucas Chitolina <lucas.chitolina@edu.pucrs.br>
2022-03-24 10:33:01 -0300

    Add files via upload

--- API/api.py
+++ API/api.py
@@ -9,6 +9,7 @@ import streamlit as st
 
 model = joblib.load('model/model.pkl')
 
+st.set_page_config(page_title="Random Forest", page_icon="🌱")
 st.sidebar.image('static/hmm.png', use_column_width=True)
 st.sidebar.title('Menu')
 pagSelecionada = st.sidebar.selectbox('Escolha uma seção',['Home','EDA', 'Previsão de vendas', 'Arquitetura do Projeto', 'Desafios', 'Equipe e Agradecimentos'])
@@ -29,7 +30,7 @@ elif pagSelecionada == 'EDA':
 
 elif pagSelecionada == 'Previsão de vendas':
     st.title("Cross-Selling: Previsão de vendas")
-    st.write("Previsão de vendas de seguro veicular á clientes de seguro de saúde")
+    st.write("Previsão de vendas de seguro veicular a clientes de seguro de saúde")
 
     ListaRegiao = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
     ListaCanais = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 73, 74, 75, 76, 78, 79, 80, 81, 82, 83, 84, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 163]
